782fab56058abffbbc0a9c63d18a5d27cb4cf8c3
chutao.wu <chutao.wu@shopee.com>
2021-10-08 20:33:07 +0800

    add

---
+++ main.py
@@ -0,0 +1,10 @@
+from brute import Brute
+import requests
+import pymysql
+if __name__ == '__main__':
+    b=Brute(domains=['vpal.com'],process=2,threads=100,file='dict/subnames_big.txt',next='dict/sub_next.txt')
+    b.run()
+
+
+
+
